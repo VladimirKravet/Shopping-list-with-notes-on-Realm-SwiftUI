cd78cc4f4acd0c2463757df385c7d626af233de1
Vladimir Kravets <natccorp@gmail.com>
2023-10-08 21:46:22 +0300

    Initial Commit

---
+++ Realm on SwiftUI app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Realm on SwiftUI app
+//
+//  Created by Vladimir Kravets on 08.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Realm on SwiftUI app/Realm_on_SwiftUI_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Realm_on_SwiftUI_appApp.swift
+//  Realm on SwiftUI app
+//
+//  Created by Vladimir Kravets on 08.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Realm_on_SwiftUI_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
